a4cced899e6a7d26970c4334137a4e202b4ab0d8
Lucas Lucena <lucasandradelucaslucas123@gmail.com>
2025-05-19 22:38:51 -0300

    Game Seed

--- dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -21,19 +21,25 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
-	public Game(Long id, String title, String genre,Integer year, String plataform, String imgUrl, String shortDescription,
+	public Game(Long id, String title, String genre,Integer year, String platforms, Double score, String imgUrl, String shortDescription,
 			String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -79,13 +85,24 @@ public class Game {
 	}
 
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlatforms(String plataforms) {
+		this.platforms = plataforms;
+	}
+	
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 
